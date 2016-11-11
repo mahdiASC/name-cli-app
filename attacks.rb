1edd2bb53fc8c8ba3150b8183fb6fd4e54536309
mahdiASC <mahdi@allstarcode.org>
2016-11-11 00:25:39 -0500

    testing attacks

--- attacks.rb
+++ attacks.rb
@@ -1,1 +1,24 @@
 # http://www.psypokes.com/rby/attacks.php
+require "nokogiri"
+require "open-uri"
+
+class Attack
+	attr_accessor :name, :desc, :type, :pp, :power, :acc
+
+	@@all=[]
+
+	def initialize (hash)
+		hash.each{|key, val| send("#{key}=", val)}
+		@@all << self
+	end
+	
+	def self.all
+		@@all
+	end
+
+	def add_by_url_parse(url)
+
+	end
+end
+
+x = Attack.new({name:"phill"})
